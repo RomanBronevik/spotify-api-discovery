cc0e2b2eeba36d186743327df65a9ae486a74f3c
Lucas Gossé <lucasgosse@MacBook-Pro-2.local>
2018-03-01 16:09:08 +0100

    [UPD] Switched on NestJS

--- server/app/api/user.js
+++
@@ -1,8 +0,0 @@
-import { app } from "../index";
-import { User } from "../schemas/User";
-import passport from "passport";
-
-app.get("/user", (req, res) => {
-  console.log("req.user:", req.user);
-  return res.send(req.user);
-});
--- server/app/database.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import environment from "../environments/environment";
-
-mongoose.connect(environment.mongodb.url, {
-  useMongoClient: true
-});
-mongoose.Promise = global.Promise;
-require("./schemas");
